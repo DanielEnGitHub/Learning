03b1bc6bb4eeee31388cdd54065f382313725219
DanielEnGitHub <danieljgallina.com>
2021-10-22 13:49:45 -0600

    Rama para frontend

--- Python/001/01-Entrada.py
+++
@@ -1,9 +0,0 @@
-resultado = input("Escriba un mensaje: ")
-print(resultado)
-print("Fin del programa")
-
-# INPUT DEVUELVE TIPO DE DATO STRING
-num1 = input("Escribe primer numero: ")
-num2 = input("Escribe segundo numero: ")
-resultado = int(num1)+ int(num2)
-print(resultado)
